70f394c95f7242e16221dc7013372bbf102185a6
Matthijs Boet <m.boet@chello.nl>
2020-06-21 18:46:53 +0200

    finished Objects section

--- Objects/10-es6-classes/app.js
+++ Objects/10-es6-classes/app.js
@@ -1,23 +1,27 @@
+/*
+
+  ES6 Classes - Syntactic Sugar for cleaner code
+  Prototypal Inheritance
+
+*/
+
 class Account {
   constructor(name, initialBalance) {
     this.name = name;
     this.balance = initialBalance;
   }
-  bank = 'Chase';
+  // no const, let or var allowed for properties
+  bank = 'CHASE'
+  // no keyword for methods aka function name()
   deposit(amount) {
     this.balance += amount;
     console.log(`Hello ${this.name}, your balance is ${this.balance}`);
   }
 }
 
+// Use new keyword and capitalization with classes
 const john = new Account('john', 0);
 console.log(john);
 console.log(john.name);
 john.deposit(500);
 console.log(john.bank);
-
-const bob = new Account('bob', 700);
-console.log(bob);
-console.log(bob.name);
-bob.deposit(1000);
-console.log(bob.bank);
