112b7abbbc6fd15c208d0d3980cee4fb5b145808
Matthijs Boet <m.boet@chello.nl>
2020-06-24 23:58:03 +0200

    learned tagged template literals in ES6 folder

--- ES6/4-tagged-template-literals/app.js
+++ ES6/4-tagged-template-literals/app.js
@@ -8,12 +8,20 @@ const statement = "Some Statement";
 const quote = highlight`Here is the ${statement} by ${author} and it could not be more true`;
 console.log(quote);
 
-function highlight(text, ...vars) {
-  let awesomeText = text.map((item, index) => {
-    return `${item} <strong class="blue">${vars[index] || ""}</strong>`;
-  });
-  return awesomeText.join("");
-}
+// function highlight(text, ...vars) {
+//   let awesomeText = text.map((item, index) => {
+//     return `${item} <strong class="blue">${vars[index] || ""}</strong>`;
+//   });
+//   return awesomeText.join("");
+// }
 
 const result = document.getElementById("result");
 result.innerHTML = quote;
+
+function highlight(text, ...args){
+  console.log({text, ...args})
+  const awesomeText = text.map((item, index) => {
+    return `${item} <strong class="blue">${args[index] || ''}</strong>`
+  });
+  return awesomeText.join("");
+}
