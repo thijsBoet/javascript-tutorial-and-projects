f2e458d4fbedcef02cde737a8421b7514a9c1910
Matthijs Boet <m.boet@chello.nl>
2020-06-24 17:32:54 +0200

    finished Funtions

--- Functions/3-closure/app.js
+++ Functions/3-closure/app.js
@@ -17,3 +17,33 @@ outer()();
 const value = outer();
 console.log(value);
 value();
+
+// basic/complete closures example
+function newAccount(name, initialBalance){
+  let balance = initialBalance;
+  function showBalance(){
+    console.log(`Hey ${name}, your balance is ${balance}`)
+  }
+  function deposit(amount){
+    balance += amount;
+    showBalance();
+  }
+  function withdraw(amount){
+    amount > balance 
+      ? console.log(`Hey ${name}, insufficient funds`)
+      : balance -= amount;
+    showBalance();
+  }
+  return { showBalance, deposit, withdraw };
+};
+
+const john = new newAccount('john', 300);
+const bob = new newAccount('bob', 1000);
+
+john.deposit(700);
+john.withdraw(1000);
+john.deposit(300);
+john.withdraw(100);
+john.withdraw(250);
+
+bob.showBalance();
