29b6574d20e192157e9860ed04dfc9ca537a3b31
Matthijs Boet <m.boet@chello.nl>
2020-06-26 19:51:55 +0200

    added modules exercise files and folder

---
+++ Modules/4-get-element/utils/getElement.js
@@ -0,0 +1,8 @@
+export default function (selection) {
+  const element = document.querySelector(selection);
+  if (element) {
+    return element;
+  } else {
+    throw Error('you did not select element');
+  }
+}
