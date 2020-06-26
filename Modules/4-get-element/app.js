67379f3e5e37d2dcda54cb3f181c03f08468db0c
Matthijs Boet <m.boet@chello.nl>
2020-06-27 01:42:33 +0200

    added excercise files for Asynchronous JS

--- Modules/4-get-element/app.js
+++ Modules/4-get-element/app.js
@@ -6,8 +6,6 @@ import get from './utils/getElement.js';
 
 const container = get('.container');
 const btn = get('.btn');
-// const container1 = get('.container1');
-// console.log(container1);
 
 btn.addEventListener('click', () => {
   container.innerHTML = showPeople(people);
