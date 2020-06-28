287e2300345e8bf117d2494c535a67f0dbf8cd68
Matthijs Boet <m.boet@chello.nl>
2020-06-29 01:28:04 +0200

    finished random person project

--- Ajax-http-json/4-random-person-project/js/app.js
+++ Ajax-http-json/4-random-person-project/js/app.js
@@ -1,46 +1,40 @@
 const btn = document.getElementById("btn");
-btn.addEventListener("click", function() {
-  getPerson(getData);
+btn.addEventListener('click', function(){
+  getPerson(getData)
 });
 
 function getPerson(cb) {
   const url = `https://randomuser.me/api/`;
   const ajax = new XMLHttpRequest();
-  ajax.open("GET", url, true);
+
+  ajax.open('GET', url, true);
 
   ajax.onload = function() {
-    if (this.status === 200) {
+    if(this.status === 200) {
       cb(this.responseText, showData);
     } else {
-      this.onerror();
+      console.log(this.statusText)
     }
-  };
+  }
 
-  ajax.onerror = function() {
-    console.log("there was an error");
-  };
-  ajax.send();
+  ajax.onerror = function (){
+    console.log('XMLHttpRequest error');
+  }
+
+  ajax.send()
 }
 
-function getData(response, cb) {
+function getData(response, cb){
   const data = JSON.parse(response);
-
-  const {
-    name: { first },
-    name: { last },
-    picture: { large },
-    location: { street },
-    phone,
-    email
-  } = data.results[0];
-
+  const {name:{first}, name:{last}, picture: {large}, location:{street}, phone, email} = data.results[0];
   cb(first, last, large, street, phone, email);
 }
+
 function showData(first, last, large, street, phone, email) {
-  document.getElementById("first").textContent = first;
+  document.getElementById('first').textContent = first;
   document.getElementById("last").textContent = last;
-  document.getElementById("street").textContent = street;
-  document.getElementById("phone").textContent = phone;
-  document.getElementById("email").textContent = email;
+  document.getElementById('street').textContent = `${street.name} ${street.number}`;
+  document.getElementById('phone').textContent = phone;
+  document.getElementById('email').textContent = email;
   document.getElementById("photo").src = large;
 }
